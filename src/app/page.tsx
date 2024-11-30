da8724593e7cc25096e062de127e3f6d1c91c721
Luciano Alves <luas10c@gmail.com>
2024-11-29 21:45:58 -0300

    chore: first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <section>
+      <div className="py-4">
+        <h2 className="text-center text-zinc-100">Hello, World!</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Home
