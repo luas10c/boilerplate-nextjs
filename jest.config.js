434db3e096d3a39d94d53e2c22c38d44e2bacfae
Luciano Alves <luas10c@gmail.com>
2024-11-29 21:45:58 -0300

    chore: first commit

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+import { default as createConfig } from 'next/jest.js'
+
+/** @type{import('jest').Config} */
+const config = {
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts', '<rootDir>/tests/msw.ts'],
+  coverageProvider: 'v8',
+  testEnvironment: './tests/environment.ts',
+  moduleNameMapper: {
+    '^#/(.*)$': '<rootDir>/src/$1'
+  }
+}
+
+export default createConfig({
+  dir: './'
+})(config)
