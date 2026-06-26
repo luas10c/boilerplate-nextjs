8f86f2ffa0c2b4e124cd01108653c3ad33270ded
Luciano Alves <luas10c@gmail.com>
2026-06-26 13:09:50 -0300

    chore: first commit

---
+++ eslint.config.js
@@ -0,0 +1,83 @@
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+import globals from 'globals'
+
+import js from '@eslint/js'
+import typescript from 'typescript-eslint'
+
+import stylistic from '@stylistic/eslint-plugin'
+
+import react from '@eslint-react/eslint-plugin'
+
+import a11y from 'eslint-plugin-a11y'
+
+import tailwindcss from 'eslint-plugin-tailwindcss'
+
+import jest from 'eslint-plugin-jest'
+import tl from 'eslint-plugin-testing-library'
+
+export default defineConfig([
+  globalIgnores(['node_modules', 'coverage', 'dist', '.next', '.vercel', '.eslintcache', './next-env.d.ts']),
+  js.configs.recommended,
+  typescript.configs.recommended,
+  {
+    name: 'stylistic/customize',
+    ...stylistic.configs.customize({
+      indent: 2,
+      quotes: 'single',
+      semi: false,
+      jsx: true,
+      arrowParens: true,
+      blockSpacing: true,
+      quoteProps: 'as-needed',
+      commaDangle: 'never',
+      braceStyle: '1tbs',
+
+      severity: 'error'
+    })
+  },
+  react.configs.recommended,
+  {
+    name: 'node/recommended',
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2024
+      }
+    }
+  },
+  {
+    name: 'a11y/recommended',
+    plugins: {
+      a11y
+    },
+    rules: {
+      ...a11y.configs.recommended.rules
+    }
+  },
+  {
+    name: 'tailwindcss/recommended',
+    plugins: {
+      tailwindcss
+    },
+    settings: {
+      tailwindcss: {
+        cssConfigPath: 'src/app/globals.css'
+      }
+    },
+    rules: {
+      ...tailwindcss.configs.recommended.rules
+    }
+  },
+  {
+    name: 'jest/recommended',
+    files: ['**/*.{spec,test}.{ts,tsx}'],
+    ...jest.configs['flat/recommended']
+  },
+  {
+    files: ['**/*.{spec,test}.{ts,tsx}'],
+    ...tl.configs['flat/react'],
+    name: 'testing-library/recommended'
+  }
+])
